303105878771c554df2acfe5bd04b8dea9163797
Abhishek Patil <abishkpatil@gmail.com>
2024-05-08 02:19:18 +0530

    feat: add counter

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,13 @@ function App() {
   return (
     <>
       <h1>Counter App</h1>
+      <div>
+        <span>Count: {count}</span>
+        <div style={{marginTop: '20px'}}>
+          <button onClick={() => setCount((prev) => prev + 1)}>Increment</button>
+          <button onClick={() => setCount((prev) => prev - 1)}>Decrement</button>
+        </div>
+      </div>
     </>
   )
 }
